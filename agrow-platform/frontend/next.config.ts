123735824cd84f631fcf02e90c6bc22949c05141
Kathiravan Devendiran <kathiravanh1919@gmail.com>
2026-04-05 22:16:52 +0530

    feat: include configuration files in modernization

---
+++ agrow-platform/frontend/next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res-console.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'doc-0s-80-docs.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
